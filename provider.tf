378e6285932d95dd1567fae3657f2543bb9f58ef
Shashwath <47598740+shashwathgm@users.noreply.github.com>
2022-07-26 23:43:32 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,17 +1,1 @@
-terraform{
-    required_version=>0.12
-    required_providers {
-        azurerm={
-            source="hashicorp/azurerm"
-            version=>1.20
-        }
-    }
-}
-
-provider "azurerm" {
-    subscription_id= "d3baa446-668f-4546-9ddb-523933f084ae"
-    client_id= "a7c4baf5-ff1c-4280-8b40-bb16a177bb6a"
-    client_secret= "jWc8Q~BxSVqY6mxhtZo6bQWqA_CIATlMdzkFQc4d"
-    tenant_id= "f3779dd8-ac70-4dd8-b326-97980777bdfa"
-}
 
