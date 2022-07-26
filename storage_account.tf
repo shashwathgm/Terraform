18861b20a784d29f3acdfbe9a0c8c9a55219e393
Shashwath <47598740+shashwathgm@users.noreply.github.com>
2022-07-26 23:44:50 +0530

    Delete storage_account.tf

--- storage_account.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_storage_account" "storageaccount" {
-    name="${var.storage}"
-    resource_group="${var.resourcegroup}"
-    location="${var.location}"
-    account_tier="standard"
-    account_replication_type = "LRS"
-    allow_blob_public_access = true
-
-}
